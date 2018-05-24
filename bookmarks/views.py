ea5abd66e9d740844e4642bdc89cbb2866b899be
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-05-24 14:01:21 -0400

    Added personal bookmarks and new bookmarks form

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
+from .forms import BookmarkForm
 # Create your views here.
 
 def index(request):
-  context = {
-    'bookmarks': Bookmark.objects.all(),
-  }
+  # import pdb; pdb.set_trace()
+
+  if request.method == 'POST':
+    form = BookmarkForm(request.POST)
+    if form.is_valid():
+      # TODO check for request.user to allow for personal bookmarks
+      form.save()
+    else:
+      # TODO error
+      pass
+
+  context = {}
+
+  pbid = PersonalBookmark.objects.values_list('id')
+
+  context['bookmarks'] = Bookmark.objects.exclude(id__in=pbid)
+
+  if request.user.is_anonymous:
+    context['personal_bookmarks'] = PersonalBookmark.objects.none()
+  else:
+    context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
+
+  context['form'] = BookmarkForm
 
   return render(request, 'bookmarks/index.html', context)
